69e09ddab12ffee9be6a5cd5b3acc0160d7b9438
lukereiner <github@mightymail.net>
2026-02-16 08:31:24 -0500

    initial commit

---
+++ 2026-02-16/script.js
@@ -0,0 +1,10 @@
+const records = ["CAN: 2-2-0-1", "FIN: 2-2-1-0", "GER: 1-0-1-3", "SUI: 0-1-3-1", "SWE: 1-1-2-1", "USA: 2-1-0-2"]
+
+function getSemifinalMatchups(teams) {
+
+    const country = {};
+    
+
+  return teams;
+}
+
